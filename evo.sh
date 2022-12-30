83e6832aef127d32f00229a079d84d1079579ee7
chunyuan666 <chunyuanmail@126.com>
2022-12-30 16:13:32 +0800

    add evo tools

--- evo.sh
+++ evo.sh
@@ -1,4 +1,4 @@
 source ~/.bashrc
-evo_traj kitti MySLAM.txt KITTI_00_ORB.txt --ref=KITTI_00_gt.txt -p --plot_mode=xz
+evo_traj kitti best.txt KITTI_00_ORB.txt --ref=KITTI_00_gt.txt -p --plot_mode=xz
 # evo_ape kitti KITTI_00_gt.txt MySLAM.txt -va --plot --plot_mode xz
 # evo_rpe kitti KITTI_00_gt.txt MySLAM.txt -va --plot --plot_mode xz
--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cd build
 cmake ..
 make -j8
 cd ..
-./bin/stereo_kitti /home/uav/A-tiny-Stereo-SLAM/config/KITTI00-02.yaml /home/uav/KITTI/02
-evo_traj kitti trajectory.txt --ref=/home/uav/KITTI/data_odometry_poses/dataset/poses/02.txt -p --plot_mode=xz
+./bin/stereo_kitti /home/uav/A-tiny-Stereo-SLAM/config/KITTI00-02.yaml /home/uav/KITTI/00
+evo_traj kitti trajectory.txt --ref=/home/uav/KITTI/data_odometry_poses/dataset/poses/00.txt -p --plot_mode=xz
